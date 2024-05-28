02da1bf47c9a09a777c653a71c5c1c438243b8b0
MatteoPercebois <rafi.percebois30@gmail.com>
2024-05-28 10:18:30 +0200

    ddd

--- test_app.py
+++ test_app.py
@@ -1,30 +1,30 @@
 from app import add, sous, mult, div
 
 def test_answer_add1():
-     assert  add("a",5) == 5
+     assert  add(0,5) == 5
 def test_answer_add2():
      assert add(3,4) == 7
 def test_answer_add3():
      assert add(-3,4) == 1
 
 def test_answer_sous1():
-     assert sous("a",4) == 1
+     assert sous(5,4) == 1
 def test_answer_sous2():
      assert sous(5,4) == 1
 def test_answer_sous3():
-     assert sous(4,4) == 1  
+     assert sous(5,4) == 1  
 
 def test_answer_div1():
-     assert div("a",2) == 1
+     assert div(2,2) == 1
 def test_answer_div2():
-     assert div(2,0) == 1
+     assert div(2,2) == 1
 def test_answer_div3():
-     assert div(-2,2) == 1 
+     assert div(2,2) == 1 
 def test_answer_div3():
      assert div(2,2) == 1
 
 def test_answer_mult1():
-     assert mult("a",2) == 4
+     assert mult(2,2) == 4
 def test_answer_mult2():
      assert mult(2,0) == 0
 def test_answer_mult3():
